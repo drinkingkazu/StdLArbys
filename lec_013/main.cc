5abbea1f1abfd725b9654e11785ab676ed272b6f
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-12-11 16:30:02 -0600

    fixed

--- lec_013/main.cc
+++ lec_013/main.cc
@@ -3,16 +3,19 @@
 
 class event_key {
 public:
-  foo(int run=0, int subrun=0, int event=0)
+  event_key(int run=0,
+	    int subrun=0,
+	    int event=0)
     : _run    ( run    )
     , _subrun ( subrun )
     , _event  ( event  )
-  ~foo() {}
+  {}
+  ~event_key() {}
 
   void set(int run, int subrun, int event)
   { _run = run; _subrun = subrun; _event = event; }
 
-  inline bool operator <(const foo& rhs) const
+  inline bool operator <(const event_key& rhs) const
   {
     if( _run < rhs._run) return true;
     if( rhs._run < _run) return false;
@@ -32,19 +35,19 @@ int main() {
 
   std::set<event_key> my_set;
 
-  my_event_keys.emplace(0,0,0);
-  my_event_keys.emplace(0,0,1);
-  my_event_keys.emplace(0,0,2);
-  my_event_keys.emplace(0,0,3);
+  my_set.emplace(0,0,0);
+  my_set.emplace(0,0,1);
+  my_set.emplace(0,0,2);
+  my_set.emplace(0,0,3);
 
   event_key key0(0,0,4);
   event_key key1(0,0,1);
 
   std::cout << "[0] My custom set created with size: " << my_set.size() << std::endl;
 
-  std::cout << "[1] My custom set contains key0 ? " << my_set.find(key0) != my_set.end() << std::endl;
+  std::cout << "[1] My custom set contains key0 ? " << (my_set.find(key0) != my_set.end()) << std::endl;
 
-  std::cout << "[2] My custom set containskey01 ? " << my_set.find(key1) != my_set.end() << std::endl;
+  std::cout << "[2] My custom set containskey01 ? " << (my_set.find(key1) != my_set.end()) << std::endl;
 
   std::cout << std::endl;
   
